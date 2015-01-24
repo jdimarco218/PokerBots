91f682bd425b14bd228297d4c8938e3d25c2150f
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-01-24 18:59:31 -0500

    Delete results.txt before run

--- headsUp.py
+++ headsUp.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
+
+import os
 import sys
-sys.path.insert(0, 'src')
-stdout = sys.stdout
-sys.stdout = open('results/results.txt', 'w')
 
 from PokerPlayerJeff import *
 from PokerPlayerOpp import *
 from HeadsUp import *
+
+# Setup file and direct prints to results/results.txt
+if os.path.isfile('./results/results.txt'):
+    os.remove('./results/results.txt')
+sys.path.insert(0, 'src')
+stdout = sys.stdout
+sys.stdout = open('./results/results.txt', 'w')
+
 
 # Create players and controlle
 pgc = PokerGameController( 1, 10000)
@@ -16,3 +23,6 @@ input_player_list = [ppOpponent, ppJeff]
 
 pgc.initGame(input_player_list)
 pgc.runGame()
+
+# Reset stdout
+sys.stdout = stdout
