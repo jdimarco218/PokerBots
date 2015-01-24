a540145d62b49a1a742f0a41d8c426ef1a2f7777
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-01-24 18:04:16 -0500

    Restructuring from test.py to headsUp.py

---
+++ headsUp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from PokerPlayerJeff import *
+from PokerPlayerOpp import *
+from HeadsUp import *
+
+# Create players and controlle
+pgc = PokerGameController( 1, 100000)
+ppOpponent = PokerPlayerOpp("Opponent", pgc)
+ppJeff = PokerPlayerJeff("Jeff", pgc)
+input_player_list = [ppOpponent, ppJeff]
+
+pgc.initGame(input_player_list)
+pgc.runGame()
